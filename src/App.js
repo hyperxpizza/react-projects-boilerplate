151f94bc2997dcecd85561ee71ee39ec427058c8
hyperxpizza <wojtek.frackowski@gmail.com>
2020-10-29 18:17:31 +0100

    cleanup app.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
-  ]);
-};
 
 const App = () => {
   return React.createElement("div", { id: "something-important" }, [
-    React.createElement("h1", {}, "Adopt me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Luna2",
-      animal: "Dog2",
-      breed: "Havanese3",
-    }),
-    React.createElement(Pet, {
-      name: "LOL",
-      animal: "Dog3",
-      breed: "Havanese3",
-    }),
+    "Hello World!"
   ]);
 };
 
